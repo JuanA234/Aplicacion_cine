30f5ba83765065648a1b101bf0b45a5a4fbc4fb6
JosephFer <150938233+JosephFer@users.noreply.github.com>
2024-11-15 15:23:44 -0500

    Delete build directory

--- build/app/generos/entity/Genero.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Genero {
-    constructor(idg, nog) {
-        this.idGenero = idg;
-        this.nombreGenero = nog;
-    }
-}
-exports.default = Genero;
