6b4d1c6cb394a582641612b007a5edd671bd7326
JosephFer <ferrerjoseph04@gmail.com>
2024-11-18 20:16:40 -0500

    Comidas bien

---
+++ src/app/comidas/Controller/ComidaControlador.ts
@@ -0,0 +1,43 @@
+import { Response, Request } from "express";
+import ComidaDAO from "../DAO/ComidaDAO";
+import Comida from "../Entity/Comida";
+
+class ComidaControlador extends ComidaDAO{
+
+    public dameComidas(req:Request, res:Response){
+        let {tamPag, page} = req.query;
+        ComidaDAO.obtenerTodo(tamPag,page,res);
+    }
+
+    public cogeTuComida(req: Request, res: Response): void{
+        const objCubi: Comida = new Comida(0,"",0);
+        objCubi.idTipoComida = req.body.idTipoComida;
+        objCubi.nombreComida = req.body.nombreComida;
+        ComidaDAO.grabeloYa(objCubi, res);
+    }
+
+    public borraTuComida(req: Request, res: Response): void {
+        if(isNaN(Number(req.params.idComida))){
+            res.status(400).json({respuesta: "Y el codigo mi vale"});
+        }else{
+            const codiguito = Number(req.params.idComida);
+            const objCubi: Comida = new Comida(codiguito,"",0);
+            ComidaDAO.borreloYa(objCubi,res);
+        }
+
+        
+    }
+
+    public actualizaTuComida(req: Request, res:Response): void{
+        const objCubi: Comida = new Comida(0,"",0);
+        objCubi.idComida = Number(req.body.idComida);
+        objCubi.idTipoComida = Number(req.body.idTipoComida);
+        objCubi.nombreComida = String(req.body.nombreComida);
+        ComidaDAO.actualiceUno(objCubi, res);
+    }
+
+}
+
+const comidaControlador = new ComidaControlador();
+
+export default comidaControlador;
---
+++ src/app/comidas/Route/ComidaRuta.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import comidaControlador from "../Controller/ComidaControlador";
+
+class ComidaRuta{
+    public apiRutaComida:Router;
+
+    constructor(){
+        this.apiRutaComida = Router();
+        this.apiRutaComida.get("/getall", comidaControlador.dameComidas);
+        this.apiRutaComida.post("/add", comidaControlador.cogeTuComida);
+        this.apiRutaComida.delete("/delete/:idComida", comidaControlador.borraTuComida);
+        this.apiRutaComida.put("/update", comidaControlador.actualizaTuComida);
+    }
+}
+
+const salaRuta = new ComidaRuta();
+export default salaRuta.apiRutaComida;
